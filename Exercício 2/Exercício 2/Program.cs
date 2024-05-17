4ddb5de5918f351a8d907f478b306b4287c399ec
lucas martins <124801819+lucasmpseven@users.noreply.github.com>
2024-05-16 21:46:19 -0300

    Add files via upload

---
+++ Exercício 2/Exercício 2/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // Exemplo de uso da classe RegistroDeComprass
+        RegistroDeCompras registro = new RegistroDeCompras();
+        registro.AdicionarCompra(DateTime.Now, "Camiseta", 29.99);
+        registro.AdicionarCompra(DateTime.Now.AddDays(-1), "Calça", 59.99);
+        registro.AdicionarCompra(DateTime.Now.AddDays(-2), "Meia", 9.99);
+
+        registro.ListarCompras();
+    }
+}
---
+++ Exercício 2/Exercício 2/RegistroDeCompras.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+class RegistroDeCompras
+{
+    // Classe interna para representar uma compraa
+    class Compra
+    {
+        public DateTime Data { get; }
+        public string Produto { get; }
+        public double Valor { get; }
+
+        public Compra(DateTime data, string produto, double valor)
+        {
+            Data = data;
+            Produto = produto;
+            Valor = valor;
+        }
+    }
+
+    // Lista de compras
+    private List<Compra> compras;
+
+    // Construtor
+    public RegistroDeCompras()
+    {
+        compras = new List<Compra>();
+    }
+
+    // Método para adicionar uma compra
+    public void AdicionarCompra(DateTime data, string produto, double valor)
+    {
+        compras.Add(new Compra(data, produto, valor));
+    }
+
+    // Método para listar as compras
+    public void ListarCompras()
+    {
+        Console.WriteLine("Lista de Compras:");
+        foreach (var compra in compras)
+        {
+            Console.WriteLine($"Data: {compra.Data.ToShortDateString()} | Produto: {compra.Produto} | Valor: R${compra.Valor}");
+        }
+    }
+}
